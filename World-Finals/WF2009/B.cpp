b59ab8a820195fd489489df03c9eb33c8806616e
CLDP <muyunhai@gmail.com>
2014-04-28 16:51:26 -0400

    Create B.cpp

--- World-Finals/WF2009/B.cpp
+++ World-Finals/WF2009/B.cpp
@@ -14,8 +14,7 @@ struct node {
 
 int N, G, U, B;
 node y[50];
-int z[50];
-int in[50];
+int z[50], res[50];
 int out[50];
 int INPUT[1000][30];
 
@@ -29,42 +28,40 @@ int getN(string P) {
     }
 }
 
+int cal(int t, int A) {
+    if (z[A]) return res[A];
+    z[A] = true;
+    if (A <= N) return res[A] = INPUT[t][A - 1];
+    int a = y[A - N].a, b = y[A - N].b;
+    switch (y[A - N].type) {
+        case 10 : return res[A] = 1;
+        case 4 : return res[A] = !cal(t, a);
+        case 3 : return res[A] = cal(t, a) ^ cal(t, b);
+        case 2 : return res[A] = cal(t, a) | cal(t, b);
+        case 1 : return res[A] = cal(t, a) & cal(t, b);
+        case -1 : return res[A] = !(cal(t, a) & cal(t, b));
+        case -2 : return res[A] = !(cal(t, a) | cal(t, b));
+        case -3 : return res[A] = !(cal(t, a) ^ cal(t, b));
+        case -4 : return res[A] = cal(t, a);
+        case -10: return res[A] = 0;
+    }
+    return 0;
+}
+
 bool check() {
-    int res[50];
     for (int t = 0; t < B; ++t) {
-        bool flag =  true;
-        for (int i = 0; i < N + G; ++i) {
-            int p = z[i];
-            if (p <= N) {
-                res[p] = INPUT[t][p - 1];
-            } else {
-                int a = y[p - N].a, b = y[p - N].b;
-                switch (y[p - N].type) {
-                    case 10 : res[p] = 1; break;
-                    case 4  : res[p] = !res[a]; break;
-                    case 3  : res[p] = res[a] ^ res[b]; break;
-                    case 2  : res[p] = res[a] | res[b]; break;
-                    case 1  : res[p] = res[a] & res[b]; break;
-                    case -1 : res[p] = !(res[a] & res[b]); break;
-                    case -2 : res[p] = !(res[a] | res[b]); break;
-                    case -3 : res[p] = !(res[a] ^ res[b]); break;
-                    case -4 : res[p] = res[a]; break;
-                    case -10: res[p] = 0; break;
-                }
-            }
-        }
-        for (int i = 0; i < U; ++i) flag &= (res[N + out[i]] == INPUT[t][N + i]);
+        bool flag = true;
+        memset(z, 0, sizeof(z));
+        for (int i = 0; i < U; ++i) flag &= (cal(t, N + out[i]) == INPUT[t][N + i]);
         if (!flag) return false;
     }
     return true;
 }
 
-
 int main() {
     int times = 0;
     while (cin >> N >> G >> U) {
         if (N == 0) break;
-        memset(in, 0, sizeof(in));
         
         for (int i = 1; i <= G; ++i) {
             char c;
@@ -75,7 +72,6 @@ int main() {
                 y[i].a = getN(p);
                 y[i].b = getN(q);
                 y[i].c = N + i;
-                in[N + i] = 2;
                 y[i].type = 1;
             } else
             if (c == 'o') {
@@ -83,7 +79,6 @@ int main() {
                 y[i].a = getN(p);
                 y[i].b = getN(q);
                 y[i].c = N + i;
-                in[N + i] = 2;
                 y[i].type = 2;
             } else
             if (c == 'x') {
@@ -91,36 +86,20 @@ int main() {
                 y[i].a = getN(p);
                 y[i].b = getN(q);
                 y[i].c = N + i;
-                in[N + i] = 2;
                 y[i].type = 3;
             } else {
                 cin >> p;
                 y[i].a = getN(p);
                 y[i].b = -1;
                 y[i].c = N + i;
-                in[N + i] = 1;
                 y[i].type = 4;
-            }
-        }
-        
-        vector<bool> Z(N + G + 2, false);
-        for (int i = 0; i < N + G; ++i) {
-            for (int j = 1; j <= N + G; ++j) {
-                if (Z[j]) continue;
-                if (in[j] > 0) continue;
-                z[i] = j;
-                Z[j] = true;
-                for (int k = 1; k <= G; ++k) {
-                    if (y[k].a == j || y[k].b == j) --in[y[k].c];
-                }
-                break;
             }
         }
         
         for (int i = 0; i < U; ++i) cin >> out[i];
         
         cin >> B;
-        for (int i = 0; i < B; ++i) 
+        for (int i = 0; i < B; ++i)
          for (int j = 0; j < N + U; ++j) cin >> INPUT[i][j];
         
         cout << "Case " << ++times << ": ";
@@ -154,7 +133,7 @@ int main() {
             cout << "Gate " << a << " is failing; ";
             switch (b) {
                 case 0 : cout << "output inverted" << endl; break;
-                case 1 : cout << "output stuck at 1" << endl; break; 
+                case 1 : cout << "output stuck at 1" << endl; break;
                 case 2 : cout << "output stuck at 0" << endl; break;
             }
         } else {
