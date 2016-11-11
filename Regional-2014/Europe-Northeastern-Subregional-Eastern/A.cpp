705132860fb444a106b9b973d00d6c0ad1d6bf7e
CLDP <muyunhai@gmail.com>
2016-11-11 15:00:32 -0500

    Create A.cpp

---
+++ Regional-2014/Europe-Northeastern-Subregional-Eastern/A.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int N;
+    cin >> N;
+    if (N >= 7) cout << "YES" << endl; else cout << "NO" << endl;
+    return 0;
+}
