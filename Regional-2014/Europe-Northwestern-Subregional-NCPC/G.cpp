6f733de58c0e8704a9633a51083c7b32cb8bf59d
CLDP <muyunhai@gmail.com>
2016-11-12 10:47:22 -0500

    Update G.cpp

--- Regional-2014/Europe-Northwestern-Subregional-NCPC/G.cpp
+++ Regional-2014/Europe-Northwestern-Subregional-NCPC/G.cpp
@@ -3,150 +3,80 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
-int temp;
-bool flag = true;
-int N, M, A, B, C;
-int x[200010];
-vector<int> X[200010];
-
-int fill(int a) {
-    for (int j = 0; j < X[a].size(); ++j) {
-        int b = X[a][j];
-        if (x[b] == x[a]) {
-            flag = false;
-            return 0;
-        }
-        if (x[b] == -x[a]) continue;
-        x[b] = -x[a];
-        if (x[b] == 1) ++temp;
-        fill(b);
-    }
-    return 0;
-}
-
-int fillzero(int a) {
-    for (int j = 0; j < X[a].size(); ++j) {
-        int b = X[a][j];
-        if (x[b] != 0) {
-            x[b] = 0;
-            fillzero(b);
-        }
-    }
-    return 0;
-}
-
-
-bool dfs(int a) {
-    if (a == N + 1) return true;
-    if (x[a] != 0) return dfs(a + 1);
-    
-    int TEMP = 1000000, SAT = 0;
-    
-    flag = true;
-    temp = 1;
-    x[a] = 1;
-    fill(a);
-    if (flag && temp < TEMP) {
-        TEMP = temp;
-        SAT = 1;
-    }
-    x[a] = 0;
-    fillzero(a);
-    
-    
-    flag = true;
-    temp = 0;
-    x[a] = -1;
-    fill(a);
-    if (flag && temp < TEMP) {
-        TEMP = temp;
-        SAT = -1;
-    }
-    fillzero(a);
-    
-    if (SAT == 0) return false;
-    
-    x[a] = SAT;
-    fill(a);
-    
-    return dfs(a + 1);
-}
-
-
+bool res[1005][1005];
 
 int main() {
-    cin >> N >> M;
-    for (int i = 0; i < M; ++i) {
-        scanf("%d%d%d", &A, &B, &C);
-        if (C == 2) {
-            if (x[A] == -1 || x[B] == -1) {
-                flag = false;
-                break;
-            }
-            x[A] = x[B] = 1;
+    int n, k;
+    int f[1005];
+    bool u[1005];
+    vector<int> e[1005];
+    
+    cin >> n >> k;
+    for (int i = 1; i <= n; ++i) {
+        cin >> f[i];
+        u[i] = false;
+        e[f[i]].push_back(i);
+    }
+    
+    vector<pair<int, int> > p;
+    for (int i = 1; i <= n; ++i) {
+        if (u[i]) continue;
+        vector<int> path;
+        int j = i;
+        while (true) {
+            bool dup = false;
+            for (int a = 0; a < path.size(); ++a)
+                if (path[a] == j) {
+                    dup = true;
+                    break;
+                }
+            if (dup) break;
+            path.push_back(j);
+            j = f[j];
         }
-        if (C == 0) {
-            if (x[A] == 1 || x[B] == 1) {
-                flag = false;
-                break;
-            }
-            x[A] = x[B] = -1;
+        int jj = j;
+        int cnt = 0;
+        queue<int> cir;
+        while (!u[jj]) {
+            cnt++;
+            cir.push(jj);
+            u[jj] = true;
+            jj = f[jj];
         }
-        if (C == 1) {
-            if (x[A] == 1) {
-                if (x[B] == 1) {
-                    flag = false;
-                    break;
+        pair<int, int> pa;
+        pa.first = cnt;
+        while (!cir.empty()) {
+            jj = cir.front();
+            cir.pop();
+            for (int a = 0; a < e[jj].size(); ++a)
+                if (!u[e[jj][a]]) {
+                    u[e[jj][a]] = true;
+                    cir.push(e[jj][a]);
+                    cnt++;
                 }
-                x[B] = -1;
-                continue;
-            }
-            if (x[A] == -1) {
-                if (x[B] == -1) {
-                    flag = false;
-                    break;
-                }
-                x[B] = 1;
-                continue;
-            }
-            if (x[B] == 1) {
-                if (x[A] == 1) {
-                    flag = false;
-                    break;
-                }
-                x[A] = -1;
-                continue;
-            }
-            if (x[B] == -1) {
-                if (x[A] == -1) {
-                    flag = false;
-                    break;
-                }
-                x[A] = 1;
-                continue;
-            }
-            X[A].push_back(B);
-            X[B].push_back(A);
+        }
+        pa.second = cnt;
+        p.push_back(pa);
+    }
+    
+    res[0][0] = true;
+    for (int i = 0; i < p.size(); ++i) {
+        for (int j = 0; j <= n; ++j) {
+            if (!res[i][j]) continue;
+            res[i + 1][j] = true;
+            for (int a = p[i].first; a <= min(p[i].second, n - j); ++a)
+                res[i + 1][j + a] = true;
         }
     }
     
-    for (int i = 1; i <= N; ++i) {
-        if (x[i] != 0 && X[i].size() != 0) fill(i);
-    }
+    int fr = 0;
+    for (int i = 1; i <= k; ++i)
+        if (res[p.size()][i]) fr = i;
     
-    flag &= dfs(1);
-    
-    int ans = 0;
-    for (int i = 1; i <= N; ++i) 
-     if (x[i] == 1) ++ans;
-    
-    if (flag) {
-        cout << ans << endl;
-    } else {
-        cout << "impossible" << endl;
-    }
+    cout << fr << endl;
 
     return 0;
 }
