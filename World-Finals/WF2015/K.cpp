79568e9988710a522eda53eb6f36ee5dd0f5bdf8
CLDP <muyunhai@gmail.com>
2016-11-18 14:53:55 -0500

    Create K.cpp

---
+++ World-Finals/WF2015/K.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+const int MAXN = 2010;
+
+vector<int> x[MAXN];
+int dfn[MAXN], low[MAXN];
+int A[MAXN], B[MAXN];
+int z[MAXN][MAXN];
+
+void bridges(int a, int f, int &times) {
+    dfn[a] = low[a] = times; 
+    int s = 0;
+    bool flag = false;
+    for (int i = 0; i < x[a].size(); ++i) {
+        if (x[a][i] == f) continue;
+        if (dfn[x[a][i]] == 0) {
+            bridges(x[a][i], a, ++times);
+            ++s;
+            low[a] = min(low[a], low[x[a][i]]);
+            if (low[x[a][i]] == dfn[x[a][i]]) z[x[a][i]][a] = z[a][x[a][i]] = true;
+        } else {
+            low[a] = min(low[a], dfn[x[a][i]]);
+        }
+    }
+}
+
+void dfs(int a, int f, int &times, int &cnt) {
+    dfn[a] = low[a] = times; 
+    int s = 0;
+    bool flag = false;
+    for (int i = 0; i < x[a].size(); ++i) {
+        if (x[a][i] == f) continue;
+        if (z[a][x[a][i]] == 1) continue;
+        if (dfn[x[a][i]] == 0) {
+            dfs(x[a][i], a, ++times, cnt);
+            ++s;
+            low[a] = min(low[a], low[x[a][i]]);
+            if (low[x[a][i]] == dfn[x[a][i]]) {
+                z[x[a][i]][a] = z[a][x[a][i]] = -1;
+                ++cnt;
+            }
+        } else {
+            low[a] = min(low[a], dfn[x[a][i]]);
+        }
+    }
+}
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int t = a % b;
+        a = b;
+        b = t;
+    }
+    return a;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    int N, M;
+    cin >> N >> M;
+    for (int i = 1; i <= M; ++i) {
+        cin >> A[i] >> B[i];
+        x[A[i]].push_back(B[i]);
+        x[B[i]].push_back(A[i]);
+    }
+    int times = 1;
+    bridges(1, -1, times);
+
+    int ans = -1;
+    for (int i = 1; i <= M; ++i) {
+        if (z[A[i]][B[i]] != 0) continue;
+        z[A[i]][B[i]] = z[B[i]][A[i]] = 1;
+        memset(dfn, 0, sizeof(dfn));
+        memset(low, 0, sizeof(low));
+        times = 1;
+        int cnt = 1;
+        dfs(A[i], -1, times, cnt);
+        z[A[i]][B[i]] = z[B[i]][A[i]] = -1;
+        if (ans == -1) ans = cnt; else ans = gcd(ans, cnt);
+    }
+
+    cout << 1;
+    for (int i = 2; i <= ans; ++i) {
+        if (ans % i == 0) cout << " " << i;
+    }
+    cout << endl;
+
+    return 0;
+}
