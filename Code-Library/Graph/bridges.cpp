992189a7fef60dc644076f528cad2cab18f57543
CLDP <muyunhai@gmail.com>
2016-11-18 15:19:51 -0500

    Update bridges.cpp

--- Code-Library/Graph/bridges.cpp
+++ Code-Library/Graph/bridges.cpp
@@ -5,13 +5,11 @@ bool z[MAXN][MAXN];
 // times is initialized to be 1
 void bridges(int a, int f, int &times) {
     dfn[a] = low[a] = times; 
-    int s = 0;
     bool flag = false;
     for (int i = 0; i < x[a].size(); ++i) {
         if (x[a][i] == f) continue;
         if (dfn[x[a][i]] == 0) {
             bridges(x[a][i], a, ++times);
-            ++s;
             low[a] = min(low[a], low[x[a][i]]);
             // This is a bridge
             if (low[x[a][i]] == dfn[x[a][i]]) z[x[a][i]][a] = z[a][x[a][i]] = true;
