b4dc052e22eb51dcb6ea373f25b7811196cf9c6f
CLDP <muyunhai@gmail.com>
2016-12-10 00:30:58 -0500

    Create gaussian_elimination_integer.cpp

---
+++ Code-Library/Math/gaussian_elimination_integer.cpp
@@ -0,0 +1,61 @@
+const int MAXN = 210;
+
+int A[MAXN][MAXN];
+int X[MAXN];
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int t = a % b;
+        a = b;
+        b = t;
+    }
+    return a;
+}
+
+void clear(int b[], int M) {
+    int k = 0;
+    bool flag = false;
+    for (int i = 0; i < M; ++i) {
+        if (b[i] == 0) continue;
+        if (flag) {
+            k = gcd(b[i], k);
+        } else {
+            k = b[i];
+            flag = true;
+        }
+    }
+    if (k == 0) return ;
+    for (int i = 0; i < M; ++i) b[i] /= k;
+}
+
+// Gaussian Elimination
+// sum A[i][j] * X[j] = A[i][M]
+// N equations, M variables
+// return 0 if no solution, otherwise 1
+// return arbitrary solution if infinitely many
+int gauss(int N, int M) {
+    for (int i = 0, r = 0; i < M; ++i) {
+        int j = r;
+        while (j < N && !A[j][i]) ++j;
+        if (j >= N) continue;
+        if (j > r) {
+            for (int k = 0; k <= M; ++k) swap(A[r][k], A[j][k]);
+        }
+        for (j = 0; j < N; ++j) {
+            if (j != r && A[j][i]) {
+                int temp = A[j][i];
+                for (int k = 0; k <= M; ++k) A[j][k] = A[j][k] * A[r][i] - temp * A[r][k];
+                clear(A[j], M + 1);
+            }
+        }
+        ++r;
+    }
+    for (int i = 0; i < N; ++i) {
+        if (!A[i][M]) continue;
+        int j = 0;
+        while (j < M && !A[i][j]) ++j;
+        if (j == M) return 0;
+        X[j] = A[i][M] / A[i][j];
+    }
+    return 1;
+}
