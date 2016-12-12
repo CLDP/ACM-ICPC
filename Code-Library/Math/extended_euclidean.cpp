f36c1de6d9f55d7e2f61b058e409ba8f8eaa9f5d
CLDP <muyunhai@gmail.com>
2016-12-12 14:26:55 -0500

    Create extended_euclidean.cpp

---
+++ Code-Library/Math/extended_euclidean.cpp
@@ -0,0 +1,23 @@
+int extended_euclidean(int a, int b, int &x, int &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int res = extended_euclidean(b, a % b, y, x);
+    y -= a / b * x;
+    return res;
+}
+
+int modular(int x, int m) {
+    if (x < 0) x += abs(x / m) * m + m;
+    return x % m;
+}
+
+// a and m must be relatively prime
+// return x such that ax = 1 mod m
+int modular_inverse(int a, int m) {
+    int x, y;
+    extended_euclidean(a, m, x, y);
+    return modular(x, m);
+}
