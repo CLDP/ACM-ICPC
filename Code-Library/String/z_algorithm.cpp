4375a6c70944af13d88cade32bba673a0aad55ac
CLDP <muyunhai@gmail.com>
2016-12-27 20:49:16 -0500

    Create z_algorithm.cpp

---
+++ Code-Library/String/z_algorithm.cpp
@@ -0,0 +1,23 @@
+// z[1 .. n-1] represents maximum length of a substring 
+// starting from i which is also a prefix of s
+void z_algorithm(char *s, int n) {
+    int L = 0, R = 0;
+    for (int i = 1; i < n; ++i) {
+        if (i > R) {
+            L = R = i;
+            while (R < n && s[R - L] == s[R]) ++R;
+            z[i] = R - L; 
+            --R;
+        } else {
+            int k = i - L;
+            if (z[k] < R - i + 1) 
+                z[i] = z[k];
+            else {
+                L = i;
+                while (R < n && s[R - L] == s[R]) ++R;
+                z[i] = R - L; 
+                --R;
+            }
+        }
+    }
+}
